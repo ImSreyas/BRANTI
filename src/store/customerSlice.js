87046daf0ae9dea2d9081aa23eb6fb390b98d29c
ImSreyas <sreyassatheesh755@gmail.com>
2023-07-17 10:56:35 +0530

    🔥 -  code base improved (store/customerSlice)

--- src/store/customerSlice.js
+++ src/store/customerSlice.js
@@ -1,26 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = JSON.parse(localStorage.getItem("customer") ?? sessionStorage.getItem("customer")) ?? {};
+const initialState =
+  JSON.parse(
+    localStorage.getItem("customer") ?? sessionStorage.getItem("customer")
+  ) ?? {};
 
 const customerSlice = createSlice({
   name: "customer",
   initialState: { value: initialState },
   reducers: {
-    setUserTemp: (state, action) => {
-      sessionStorage.setItem("customer", JSON.stringify(action.payload));
-      state.value = action.payload;
-    },
-    setUserPermanent: (state, action) => {
-      localStorage.setItem("customer", JSON.stringify(action.payload));
-      state.value = action.payload;
+    setUser: (state, action) => {
+      if (action.payload.type == "TEMPORARY") {
+        sessionStorage.setItem(
+          "customer",
+          JSON.stringify(action.payload.value)
+        );
+      } else {
+        localStorage.setItem("customer", JSON.stringify(action.payload.value));
+      }
+      state.value = action.payload.value;
     },
     unsetUser: (state) => {
       localStorage.removeItem("customer");
-      sessionStorage.removeItem("customer")
+      sessionStorage.removeItem("customer");
       state.value = {};
     },
   },
 });
 
-export const { setUserTemp, setUserPermanent, unsetUser } = customerSlice.actions;
+export const { setUser, unsetUser } = customerSlice.actions;
 export default customerSlice.reducer;
